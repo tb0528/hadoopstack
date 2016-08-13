462440646e822ee1b55bc5d1f6d19d711f12da46
jasonTangxd <jason.tangxd@gmail.com>
2016-08-13 13:20:01 +0800

    RPC远程过程调用

---
+++ hadoop_rpc_server/src/main/java/com/xxo/rpc/ClientNameNodeProtocol.java
@@ -0,0 +1,11 @@
+package com.xxo.rpc;
+
+/**
+ * HDFS客户端跟NameNode的远程过程调用使用的协议（接口）
+ * Created by xiaoxiaomo on 2016/8/13.
+ */
+public interface ClientNameNodeProtocol {
+
+    public static final long versionID = 11;
+    public String getMetaData(String path) ;
+}
--- hadoop_rpc_server/src/main/java/com/xxo/rpc/PublishServer.java
+++ hadoop_rpc_server/src/main/java/com/xxo/rpc/PublishServer.java
@@ -2,6 +2,7 @@ package com.xxo.rpc;
 
 import com.xxo.rpc.LoginServiceInterface;
 import com.xxo.rpc.imp.LoginServiceImp;
+import com.xxo.rpc.imp.NameNodeNameSystem;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.ipc.RPC;
 
@@ -16,17 +17,36 @@ public class PublishServer {
     public static void main(String[] args) throws IOException {
 
         //1. 创建一个RPC Builder,并设置参数（IP、端口、接口和实现类）
-        RPC.Builder builder = new RPC.Builder(new Configuration());
-        builder.setBindAddress("xxo07")
+        RPC.Builder builder1 = new RPC.Builder(new Configuration());
+        builder1.setBindAddress("xxo07")
                 .setPort(1888)
                 .setProtocol(LoginServiceInterface.class)
                 .setInstance(new LoginServiceImp()) ;
 
         //2. 调用build()方法创建
-        RPC.Server server = builder.build();
+        RPC.Server server1 = builder1.build();
 
         //3. 启动一个RPC服务
-        server.start();
+
+        System.out.println("server1 启动了。。。。。。。。。。。。");
+        server1.start();
+
+
+
+
+        //NameNode
+        RPC.Builder builder2 = new RPC.Builder(new Configuration());
+        builder2.setBindAddress("xxo07")
+                .setPort(1889)
+                .setProtocol(ClientNameNodeProtocol.class)
+                .setInstance(new NameNodeNameSystem()) ;
+
+        //2. 调用build()方法创建
+        RPC.Server server2 = builder2.build();
+
+        //3. 启动一个RPC服务
+        System.out.println("server2 启动了。。。。。。。。。。。。");
+        server2.start();
 
     }
 
